a9ce003c8dddb8b40a330303d11b4195c2131524
Andre Bagus Setiawan <154858814+andrebagus18@users.noreply.github.com>
2026-06-27 22:47:12 +0700

    edit route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -59,7 +59,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect()->route('auth.login');
+        return redirect('/auth/login');
     }
     public function showLogin()
     {
